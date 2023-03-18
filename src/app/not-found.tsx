3cdb421abf807de85632ffa21825b8ba5d7d71eb
pooh <cktjdgus45@naver.com>
2023-03-18 19:46:39 +0900

    not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const notFoundPage = () => {
+    return (
+        <>해당 제품을 찾을 수 없습니다 404</>
+    )
+}
+
+export default notFoundPage;
--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import { notFound } from 'next/navigation';
+
 type Props = {
     params: {
         slug: string;
     }
 }
 const slug = ({ params }: Props) => {
+    if (params.slug === 'nothing') {
+        notFound();
+    }
     return (
         <>
             {params.slug} | product의 기본골격 페이지입니다 !
         </>
     )
+}
+//어떤 경로라도 동적으로 접근할수있기때문에 페이지를 미리 만들수가없었다.
+//어떤 경로에 한해서 미리 페이지를 만들수 있게 하기.
+//NEXTJS에게 함수로알려주기.
+export function generateStaticParams() {
+    const products = ['pants', 'skirt'];
+    return products.map(product => ({
+        slug: product
+    }))
 }
 
 export default slug;
