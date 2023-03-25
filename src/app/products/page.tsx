be06f140e2c2b3a3a2dd27bde402680fcecfabaf
pooh <cktjdgus45@naver.com>
2023-03-25 16:04:50 +0900

    ver2.0

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -14,7 +14,7 @@ const ProductsPage = async () => {
 
     return (
         <>
-            <h1>제품 소개 페이지 입니다.</h1>
+            <h1>제품 소개 페이지 입니다 ver2..</h1>
             <Image src={clothesImage} alt="Clothes" priority />
             <ul>
                 {products.map(({ id, name }, index) => (
